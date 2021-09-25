64d3a27bfc797baaa014f40c84943f96b28026b6
lakhankumar <lakhankumar997@gmail.com>
2021-09-25 19:03:59 +0530

    Anaslyse whether the mood is happy or sad

--- main/java/MoodAnalyser.java
+++ main/java/MoodAnalyser.java
@@ -1,13 +1,9 @@
+
 public class MoodAnalyser {
     public String analyserMood(String message){
         if(message.toLowerCase().contains("sad")){
             return "SAD";
         }
         return "happy";
-    }
-
-    public static void main(String[] args) {
-        MoodAnalyser moodAnalyser=new MoodAnalyser();
-        moodAnalyser.analyserMood("i am happy");
     }
 }
