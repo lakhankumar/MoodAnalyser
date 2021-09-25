b68666be7dfe5ee3908415a2dc2183ee8e4caad2
lakhankumar <lakhankumar997@gmail.com>
2021-09-25 20:37:12 +0530

    Exception handling using custom exception

--- main/java/MoodAnalyser.java
+++ main/java/MoodAnalyser.java
@@ -8,12 +8,17 @@ public class MoodAnalyser {
 
     public String analyserMood() {
         try {
+            if (message.length() == 0) {
+                throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.ENTERED_EMPTY,
+                        "Enter Proper Message. EMPTY Not Allowed");
+            }
             if (message.toLowerCase().contains("sad")) {
                 return "SAD";
             }
             return "HAPPY";
         } catch (NullPointerException exception) {
-            return "HAPPY";
+            throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.ENTERED_NULL,
+                    "Enter Proper Message. NULL Not Allowed");
         }
     }
 }
--- test/java/MoodAnalyserTest.java
+++ test/java/MoodAnalyserTest.java
@@ -4,23 +4,30 @@ import org.junit.Test;
 public class MoodAnalyserTest {
 
     @Test
-    public void givenSadMood_ShouldReturnSad(){
+    public void givenSadMood_ShouldReturnSad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("i am sad");
         String result = moodAnalyser.analyserMood();
         Assert.assertEquals("SAD", result);
     }
 
     @Test
-    public void givenAnyMood_ShouldReturnHappy(){
+    public void givenAnyMood_ShouldReturnHappy() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("i am happy");
         String result = moodAnalyser.analyserMood();
         Assert.assertEquals("HAPPY", result);
     }
 
-    @Test
-    public void  givenMessage_WhenNull_ShouldReturnHappy(){
+    @Test(expected = MoodAnalyserException.class)
+    public void givenMessage_WhenNull_ShouldReturnNull() throws MoodAnalyserException {
         MoodAnalyser moodAnalyser = new MoodAnalyser(null);
         String result = moodAnalyser.analyserMood();
-        Assert.assertEquals("HAPPY", result);
+        Assert.assertEquals("Enter Proper Message. NULL Not Allowed", result);
+    }
+
+    @Test(expected = MoodAnalyserException.class)
+    public void givenMessage_WhenEmpty_ShouldReturnEmpty() throws MoodAnalyserException {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("");
+        String result = moodAnalyser.analyserMood();
+        Assert.assertEquals("Enter Proper Message. EMPTY Not Allowed", result);
     }
 }
